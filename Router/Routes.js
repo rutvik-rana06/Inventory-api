100d7e0abd11e8d5d8df789c7651f64b96d26251
rutvik-rana06 <121786572+rutvik-rana06@users.noreply.github.com>
2023-03-02 15:12:47 +0530

    feat: completed with addHardware api

--- Router/Routes.js
+++ Router/Routes.js
@@ -3,5 +3,6 @@ const addHardware = require("../Controller/addhardware.js")
 
 const router = express.Router()
 router.post("/addhardware",addHardware);
+// router.get("/gethardware",showHardware);
 
 module.exports = router;
--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const cors = require('cors');
 const app = express()
 const port = 5000
 
+
+app.use(express.json())
 app.use(cors());
 app.use("/",router);
 
